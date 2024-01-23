98273249293c4a3b5ad67df51d599e4fd68f353c
RazvanRadu2208 <razvanradu2208@gmail.com>
2024-01-23 17:58:52 +0200

    ...

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,27 @@
-import { Work_Sans } from 'next/font/google'
-import './globals.css'
+import { Work_Sans } from 'next/font/google';
+import './globals.css';
 
-const workSans = Work_Sans({ subsets: ['latin'] })
+const workSans = Work_Sans({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Pixellab Ecom Upsalla',
   description: 'Our first ecommerce app',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={workSans.className}>{children}</body>
+      <body className={workSans.className}>
+        <div className="layout-grid">
+          <header className="header">header</header>
+
+          <div className="main-area">
+            <main className="content">{children}</main>
+
+            <footer className="footer">footer</footer>
+          </div>
+        </div>
+      </body>
     </html>
-  )
+  );
 }
