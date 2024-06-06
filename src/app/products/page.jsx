9a4a5de121e1079f2dd7f8b8121f7fbed73e2059
RazvanRadu2208 <razvanradu2208@gmail.com>
2024-06-06 20:57:10 +0300

    Update page.jsx

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,31 +1,58 @@
 import { BackToShop, CartControls } from '@/components/cart/client';
 import { baseUrl } from '@/index';
-import { redirect } from 'next/dist/server/api-utils';
+import { redirect } from 'next/navigation';
 
-const getProduct = async (productId) => {
-  return fetch(`${baseUrl}/products/${productId}`)
-    .then((response) => {
-      return response.json();
-    })
-    .catch(() => {
+const fetchProduct = async (productId) => {
+  try {
+    const res = await fetch(`${baseUrl}/products/${productId}`);
+
+    console.log('Response status:', res.status);
+    console.log('Response headers:', res.headers);
+
+    if (!res.ok) {
+      console.error(`Fetch error: ${res.statusText}`);
       redirect('/not-found');
-    });
+      return;
+    }
+
+    const text = await res.text();
+    console.log('Response body:', text);
+
+    if (!text) {
+      console.error('Empty response body');
+      console.log(res.body);
+      redirect('/not-found');
+      return;
+    }
+
+    const product = JSON.parse(text);
+    return product;
+  } catch (error) {
+    console.error('Failed to fetch product:', error);
+    redirect('/not-found');
+  }
 };
 
-export default async function ProductPage({ params }) {
+const ProductPage = async ({ params }) => {
   const productId = params.pid;
-  const product = await getProduct(productId);
+  const product = await fetchProduct(productId);
+
+  if (!product) {
+    // This should theoretically never be hit because of the redirects above
+    return null;
+  }
 
   return (
     <div className="container px-4 mx-auto">
       <header className="flex justify-between">
         <BackToShop></BackToShop>
-
         <CartControls></CartControls>
       </header>
 
-      <section>product: {productId}</section>
+      <section>Product: {productId}</section>
       <section>{JSON.stringify(product)}</section>
     </div>
   );
-}
+};
+
+export default ProductPage;
